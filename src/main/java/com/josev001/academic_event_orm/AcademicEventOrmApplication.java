cbb29dec7ca84f72dff20ef0801de19d73f4d429
JoseV-001 <Leaphaz@gmail.com>
2025-12-29 18:40:34 -0300

    adiciona entidade Categoria e application-test.properties

---
+++ src/main/java/com/josev001/academic_event_orm/AcademicEventOrmApplication.java
@@ -0,0 +1,13 @@
+package com.josev001.academic_event_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicEventOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicEventOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josev001/academic_event_orm/AcademicEventOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josev001.academic_event_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicEventOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
